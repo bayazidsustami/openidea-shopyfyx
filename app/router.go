0ec19599d59811645126348f449be8ca0069fb3d
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-11 22:24:21 +0800

    feat : inject into router

--- app/router.go
+++ app/router.go
@@ -2,12 +2,14 @@ package app
 
 import (
 	"openidea-shopyfyx/controller/user_controller"
+	"openidea-shopyfyx/service/user_service"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func RegisterRoute(app *fiber.App) {
-	userController := user_controller.New()
+	userService := user_service.New()
+	userController := user_controller.New(userService)
 
 	userGroup := app.Group("/v1/user")
 	userGroup.Post("/register", userController.Register)
