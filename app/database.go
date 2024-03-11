bb2ea8ffd1908618848cb5291f765288cdbcc2ae
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-11 16:12:31 +0800

    chore : update db connection

--- app/database.go
+++ app/database.go
@@ -2,25 +2,34 @@ package app
 
 import (
 	"context"
+	"fmt"
 	"time"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func GetDBConnection(context context.Context) *pgxpool.Pool {
-	config, err := pgx.ParseConfig("postgres://user:password@localhost:5432/database_name")
+const (
+	DB_NAME     = "openidea_shopifyx"
+	DB_USERNAME = "root"
+	DB_PASSWORD = "admpassword"
+	DB_HOST     = "localhost"
+	DB_PORT     = "5432"
+)
+
+func GetDBConnection(context context.Context) *pgxpool.Conn {
+	dbUrl := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+	config, err := pgxpool.ParseConfig(dbUrl)
+
+	config.MaxConnLifetime = 60 * time.Minute
+	config.MaxConnIdleTime = 5 * time.Minute
+	config.MaxConns = 100
+	config.MinConns = 10
+
 	if err != nil {
 		panic(err)
 	}
 
-	dbPool, err := pgxpool.NewWithConfig(context, &pgxpool.Config{
-		ConnConfig:      config,
-		MaxConnLifetime: 60 * time.Minute,
-		MaxConnIdleTime: 5 * time.Minute,
-		MaxConns:        100,
-		MinConns:        10,
-	})
+	dbPool, err := pgxpool.NewWithConfig(context, config)
 
 	if err != nil {
 		panic(err)
@@ -28,5 +37,12 @@ func GetDBConnection(context context.Context) *pgxpool.Pool {
 
 	defer dbPool.Close()
 
-	return dbPool
+	conn, err := dbPool.Acquire(context)
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Release()
+
+	return conn
 }
