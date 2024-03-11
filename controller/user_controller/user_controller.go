1fec8c48c11f60d995321aaf4a5450091bbe6f30
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-11 21:56:12 +0800

    feat : register user endpoints

---
+++ controller/user_controller/user_controller.go
@@ -0,0 +1,22 @@
+package usercontroller
+
+import "github.com/gofiber/fiber/v2"
+
+type UserController struct {
+}
+
+func New() UserController {
+	return UserController{}
+}
+
+func (controller *UserController) Register(ctx *fiber.Ctx) error {
+	//TODO : parsing request body and do register
+
+	return nil
+}
+
+func (controller *UserController) Login(ctx *fiber.Ctx) error {
+	// TODO : parsing request body and do login
+
+	return nil
+}
