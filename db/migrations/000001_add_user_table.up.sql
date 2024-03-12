8e09cd9104a1d6f7c741a2a7e3406e0ce8778132
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-12 13:42:35 +0800

    chore : update timestamp with current create

--- db/migrations/000001_add_user_table.up.sql
+++ db/migrations/000001_add_user_table.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
 );
