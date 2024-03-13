e713f4fa20a7f93f597c8635fd32edd37549aa6e
Fiqar Aprialim <fiqarapr@gmail.com>
2024-03-13 21:24:47 +0700

    fix: change bank_accounts table migration

--- db/migrations/000004_add_bank_accounts_table.down.sql
+++ db/migrations/000004_add_bank_accounts_table.down.sql
@@ -1,9 +1,1 @@
-CREATE TABLE IF NOT EXISTS bank_accounts (
-    bank_account_id SERIAL PRIMARY KEY,
-    bank_account_name VARCHAR(255) NOT NULL,
-    bank_account_number VARCHAR(255) NOT NULL,
-    bank_name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (user_id)
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP
-);
+DROP TABLE IF EXISTS bank_accounts;
