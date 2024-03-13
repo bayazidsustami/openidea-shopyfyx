8dc1f3f5dce18edf63f4e0248648d01b8f944790
Fiqar Aprialim <fiqarapr@gmail.com>
2024-03-13 21:43:56 +0700

    feat: bank_account models

---
+++ models/bank_account/bank_account_entity.go
@@ -0,0 +1,9 @@
+package bank_account_model
+
+type BankAccount struct {
+	BankAccountId     int
+	BankName          string
+	BankAccountName   string
+	BankAccountNumber string
+	UserId            int
+}
