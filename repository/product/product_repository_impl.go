99fc2d53789542b7e8bfe65696257ddb7c135936
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-13 16:08:25 +0800

    feat : delete products

--- repository/product/product_repository_impl.go
+++ repository/product/product_repository_impl.go
@@ -46,7 +46,7 @@ func (repository *ProductRepositoryImpl) Create(ctx context.Context, tx pgx.Tx, 
 
 func (repository *ProductRepositoryImpl) Update(ctx context.Context, tx pgx.Tx, product product_model.Product) product_model.Product {
 	PRODUCT_UPDATE := "UPDATE products " +
-		"SET product_name = $1,  price = $2, image_url = $3, tags = $4, is_available=$5 " +
+		"SET product_name = $1,  price = $2, image_url = $3, tags = $4, is_available=$5, update_at = CURRENT_TIMESTAMP" +
 		"WHERE product_id = $6 AND user_id = $7"
 
 	productTags := strings.Join(product.Tags, ",")
@@ -66,7 +66,9 @@ func (repository *ProductRepositoryImpl) Update(ctx context.Context, tx pgx.Tx, 
 }
 
 func (repository *ProductRepositoryImpl) Delete(ctx context.Context, tx pgx.Tx, productId int) {
-
+	PRODUCT_DELETE := "UPDATE products SET delete_at = CURRENT_TIMESTAMP WHERE product_id=$1"
+	_, err := tx.Exec(ctx, PRODUCT_DELETE, productId)
+	utils.PanicErr(err)
 }
 func (repository *ProductRepositoryImpl) GetAllProduct(ctx context.Context, tx pgx.Tx) []product_model.Product {
 	return []product_model.Product{}
