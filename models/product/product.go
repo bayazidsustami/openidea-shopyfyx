996321a9df18c3bcb5fc583761e60d8ba0f0c40d
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-13 13:16:52 +0800

    feat : create product model

---
+++ models/product/product.go
@@ -0,0 +1,23 @@
+package product_model
+
+import "time"
+
+type Product struct {
+	ProductId   int
+	ProductName string
+	Condition   Condition
+	Tags        []string
+	IsAvailable bool
+	ImageUrl    string
+	UserId      int
+	CreatedAt   *time.Time
+	DeletedAt   *time.Time
+	UpdatedAt   *time.Time
+}
+
+type Condition string
+
+const (
+	New    Condition = "new"
+	Second Condition = "second"
+)
