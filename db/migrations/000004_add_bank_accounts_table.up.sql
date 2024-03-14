98548f2dcca7135ba637d12d81b5f3e0b13cc1f9
Fiqar Aprialim <fiqarapr@gmail.com>
2024-03-14 21:18:44 +0700

    fix: migration table bank_accounts

--- db/migrations/000004_add_bank_accounts_table.up.sql
+++ db/migrations/000004_add_bank_accounts_table.up.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS bank_accounts (
     bank_account_name VARCHAR(255) NOT NULL,
     bank_account_number VARCHAR(255) NOT NULL,
     bank_name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (user_id)
+    user_id SERIAL NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP
+    updated_at TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
