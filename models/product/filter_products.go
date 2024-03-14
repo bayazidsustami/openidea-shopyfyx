560f7d0d2050e904cbf8b8808e0fb2b399043e7e
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-14 12:54:48 +0800

    chore : add validation

--- models/product/filter_products.go
+++ models/product/filter_products.go
@@ -1,15 +1,15 @@
 package product_model
 
 type FilterProducts struct {
-	UserOnly       bool      `json:"userOnly"`
-	Limit          int       `json:"limit"`
-	Offset         int       `json:"offset"`
-	Tags           []string  `json:"tags"`
-	Condition      Condition `json:"condition"`
-	ShowEmptyStock bool      `json:"showEmptyStock"`
-	MaxPrice       float64   `json:"maxPrice"`
-	MinPrice       float64   `json:"minPrice"`
-	SortBy         string    `json:"sortBy"`
-	OrderBy        string    `json:"orderBy"`
+	UserOnly       bool      `json:"userOnly" validate:"boolean"`
+	Limit          int       `json:"limit" validate:"number"`
+	Offset         int       `json:"offset" validate:"number"`
+	Tags           []string  `json:"tags" validate:"dive"`
+	Condition      Condition `json:"condition" validate:"oneof=new second ''"`
+	ShowEmptyStock bool      `json:"showEmptyStock" validate:"boolean"`
+	MaxPrice       float64   `json:"maxPrice" validate:"number"`
+	MinPrice       float64   `json:"minPrice" validate:"number"`
+	SortBy         string    `json:"sortBy" validate:"oneof=price date ''"`
+	OrderBy        string    `json:"orderBy" validate:"oneof=asc dsc ''"`
 	Search         string    `json:"search"`
 }
