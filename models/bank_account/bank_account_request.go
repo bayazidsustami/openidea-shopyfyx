9fb69f5b649307986006429210e970cf1c2986d6
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-15 04:42:13 +0800

    feat : change json payload keys

--- models/bank_account/bank_account_request.go
+++ models/bank_account/bank_account_request.go
@@ -1,7 +1,7 @@
 package bank_account_model
 
 type BankAccountRequest struct {
-	BankName          string `json:"bank_name" validate:"required,min=3,max=30"`
-	BankAccountName   string `json:"bank_account_name" validate:"required,min=3,max=15"`
-	BankAccountNumber string `json:"bank_account_number" validate:"required,min=10,max=19"`
+	BankName          string `json:"bankName" validate:"required,min=3,max=30"`
+	BankAccountName   string `json:"bankAccountName" validate:"required,min=3,max=15"`
+	BankAccountNumber string `json:"bankAccountNumber" validate:"required,min=10,max=19"`
 }
