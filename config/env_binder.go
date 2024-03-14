b7aa120f95b599a255bba91e5a8ed5e70d67c2ef
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-15 01:16:30 +0800

    feat : add config env

--- config/env_binder.go
+++ config/env_binder.go
@@ -23,9 +23,16 @@ func envBinder() {
 	viper.BindEnv("DB_PORT")
 
 	/**
-	* Env variables related to AWS S3
+	 * Env variables related to AWS S3
 	 */
 	viper.BindEnv("S3_ID")
 	viper.BindEnv("S3_SECRET_KEY")
 	viper.BindEnv("S3_BUCKET_NAME")
+
+	/**
+	 * Env variable related to encrypt and hash
+	 */
+	viper.BindEnv("BCRYPT_SALT")
+	viper.BindEnv("JWT_SECRET")
+
 }
