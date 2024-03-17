fd795340ccff6ddf7a3cfb09c87f4b4bcff173cc
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-17 22:39:16 +0700

    fix : add number validation

--- models/bank_account/bank_account_request.go
+++ models/bank_account/bank_account_request.go
@@ -3,5 +3,5 @@ package bank_account_model
 type BankAccountRequest struct {
 	BankName          string `json:"bankName" validate:"required,min=5,max=15"`
 	BankAccountName   string `json:"bankAccountName" validate:"required,min=5,max=15"`
-	BankAccountNumber string `json:"bankAccountNumber" validate:"required,min=5,max=15"`
+	BankAccountNumber string `json:"bankAccountNumber" validate:"required,number,min=5,max=15"`
 }
